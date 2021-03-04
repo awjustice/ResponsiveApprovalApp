af70c05cb35c44df9edfc9dd79cbe2d121b6b158
Alan Justice <awjustice@gmail.com>
2021-03-03 22:13:31 -0800

    looking real good

--- app.js
+++ app.js
@@ -6,36 +6,46 @@ const voteBarChocolate1 = document.querySelector("#voteBarChocolate1");
 const voteBarChocolate2 = document.querySelector("#voteBarChocolate2");
 const voteBarVanilla = document.querySelector("#voteBarVanilla");
 const voteBars = document.querySelectorAll(".voteBar");
+const radios = document.querySelectorAll("input");
+const chocolate2 = document.querySelector("#chocolate2");
 
 const homeReload = () => {
-  document.location.href = "index.html";
+    document.location.href = "index.html";
 };
 
 voteButton.addEventListener("click", () => {
-  mainText.innerHTML = explanation[0];
-  images.forEach((image) => image.classList.add("noHover"));
-  moreButton.classList.toggle("hide");
-  voteButton.classList.toggle("hide");
+    if (!(radios[0].checked || radios[1].checked || radios[2].checked)) {
+        alert("Choose an ice cream!");
+        return false;
+    }
+
+    mainText.innerHTML = explanation[0];
+    images.forEach((image) => image.classList.add("noHover"));
+    moreButton.classList.toggle("hide");
+    voteButton.classList.toggle("hide");
+    images[2].classList.add("winner");
+    images[0].style.border = "none";
+    images[1].style.border = "none";
 });
 
 let clicks = 0;
 let explanation = [
-  "Vanilla wins in this case...even though most people prefer some form of chocolate",
-  "The chocolate lovers split their vote between the two choices.  This happens all the time in real elections.",
-  "Approval voting lets you vote for all the candidates you approve of so as to avoid this negative scenario",
-  "",
+    "Vanilla wins in this case...even though most people prefer some form of chocolate",
+    "The chocolate lovers split their vote between the two choices.  This happens all the time in real elections.",
+    "Approval voting lets you vote for all the candidates you approve of so as to avoid this negative scenario",
+    "",
 ];
 
 const infoTextChange = () => {
-  clicks += 1;
-  mainText.innerHTML = explanation[clicks];
-  if (clicks == 1) {
-    moreButton.innerHTML = "Show me a better way";
-  }
-  if (clicks == 2) {
-    moreButton.innerHTML = "Let's try it!";
-  }
-  if (clicks == 3) {
-    document.location.href = "approval.html";
-  }
+    clicks += 1;
+    mainText.innerHTML = explanation[clicks];
+    if (clicks == 1) {
+        moreButton.innerHTML = "Show me a better way";
+    }
+    if (clicks == 2) {
+        moreButton.innerHTML = "Let's try it!";
+    }
+    if (clicks == 3) {
+        document.location.href = "approval.html";
+    }
 };
--- approval.js
+++ approval.js
@@ -1,38 +1,51 @@
-const chocolate1 = document.querySelector("#chocolate1");
-const chocolate2 = document.querySelector("#chocolate2");
-const vanilla = document.querySelector("#vanilla");
+const images = document.querySelectorAll("img");
 const voteButton = document.querySelector("#voteButton");
 const mainText = document.querySelector("#mainText");
 const moreButton = document.querySelector("#moreButton");
 const wrappers = document.querySelectorAll(".wrapper");
+const checkboxes = document.querySelectorAll("input");
 
 const homeReload = () => {
-  document.location.href = "/";
+    document.location.href = "/";
 };
 
 let clicks = 0;
 let explanation = [
-  "Now one of the chocolates emerges victorious!  A more accurate reflection of voters' true preference...",
-  "The undesirable vote-splitting effect has been avoided - no one candidate is able to throw things off and 'spoil' the election",
-  "To learn more, go to...",
+    "Now one of the chocolates is victorious!  A more accurate reflection of voters' true preference...",
+    "The undesirable vote-splitting effect has been avoided - no one candidate is able to spoil the election",
+    "To learn more, go to...",
 ];
 
 voteButton.addEventListener("click", (e) => {
-  mainText.innerHTML = explanation[0];
-  voteButton.classList.toggle("hide");
-  moreButton.classList.toggle("hide");
-  wrappers.forEach((wrapper) => wrapper.classList.add("noHover"));
+    if (
+        !(
+            checkboxes[0].checked ||
+            checkboxes[1].checked ||
+            checkboxes[2].checked
+        )
+    ) {
+        alert("Choose your ice cream(s)!");
+        return false;
+    }
+
+    mainText.innerHTML = explanation[0];
+    voteButton.classList.toggle("hide");
+    moreButton.classList.toggle("hide");
+    wrappers.forEach((wrapper) => wrapper.classList.add("noHover"));
+    images[1].classList.add("winner");
+    images[0].style.border = "none";
+    images[2].style.border = "none";
 });
 
 moreButton.addEventListener("click", () => {
-  clicks += 1;
+    clicks += 1;
 
-  if (clicks == 1) {
-    mainText.innerHTML = explanation[1];
-  }
-  if (clicks == 2) {
-    mainText.innerHTML = explanation[2];
-    moreButton.innerHTML =
-      "<a target=_blank href='https://www.electionscience.org'>ElectionScience.org</a>";
-  }
+    if (clicks == 1) {
+        mainText.innerHTML = explanation[1];
+    }
+    if (clicks == 2) {
+        mainText.innerHTML = explanation[2];
+        moreButton.innerHTML =
+            "<a target='_blank' rel='noopener noreferrer' href='https://www.electionscience.org'>ElectionScience.org</a>";
+    }
 });
